25baa68cc494d7a8ab369f96b895cfbf92e34353
Sean Smith <seansmith1020@gmail.com>
2014-06-19 14:14:20 -0600

    prints the first date of correspondence with Joe

--- 05_joes_date.rb
+++ 05_joes_date.rb
@@ -1,3 +1,9 @@
 require_relative 'people'
 
 # What was the date of the first correspondence with joe?
+
+if PEOPLE["joe"][:correspondence][0][:date] < PEOPLE["joe"][:correspondence][1][:date]
+  puts PEOPLE["joe"][:correspondence][0][:date]
+else
+  puts PEOPLE["joe"][:correspondence][1][:date]
+end
