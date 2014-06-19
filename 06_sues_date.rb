fd0c13250898c16ff8172974c6ed8a798ef5fa24
Sean Smith <seansmith1020@gmail.com>
2014-06-19 14:16:15 -0600

    prints description for the last correspondence received from sue

--- 06_sues_date.rb
+++ 06_sues_date.rb
@@ -1,3 +1,9 @@
 require_relative 'people'
 
 # What was the description of the last correspondence with sue?
+
+if PEOPLE["sue"][:correspondence][0][:date] > PEOPLE["sue"][:correspondence][1][:date]
+  puts PEOPLE["sue"][:correspondence][0][:description]
+else
+  puts PEOPLE["sue"][:correspondence][1][:description]
+end
