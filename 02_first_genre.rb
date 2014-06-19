2eaf7eaad80839cb41e1a2e3576ec0513600b190
Sean Smith <seansmith1020@gmail.com>
2014-06-19 14:05:45 -0600

    printing favorite musical genres

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,6 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+
+puts PEOPLE["joe"][:preferences][:favorite_genres]
+puts PEOPLE["sue"][:preferences][:favorite_genres]
